eb84256911b588fd62b078a9a2b0978d314d6e0a
RoldanUriel <uriroldan06@gmail.com>
2025-09-24 16:15:35 -0600

    1 creacion de proyecto

---
+++ APLICACION APPLE/APLICACION-APPLE/src/test/java/com/tienda/APLICACION/APPLE/AplicacionAppleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda.APLICACION.APPLE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionAppleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
